0f75ad32613634ae0b75c467f8f734b54ef553b0
mvglasow <michael -at- vonglasow.com>
2017-06-03 18:26:50 +0200

    AlertC: getter method for message timestamp
    Signed-off-by: mvglasow <michael -at- vonglasow.com>

--- RDSSurveyor/src/eu/jacquet80/rds/app/oda/AlertC.java
+++ RDSSurveyor/src/eu/jacquet80/rds/app/oda/AlertC.java
@@ -1483,6 +1483,13 @@ public class AlertC extends ODA {
 			return result;
 		}
 		
+		/**
+		 * @brief Returns the date and time at which this message was received.
+		 */
+		public Date getTimestamp() {
+			return (Date) date.clone();
+		}
+		
 		public int getUpdateCount() {
 			return updateCount;
 		}
