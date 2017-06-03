bf08206c8ca51d9296415fce246c478c2a236bbb
mvglasow <michael -at- vonglasow.com>
2017-06-03 19:10:57 +0200

    TMC: Fix NPE when receiving an undefined Supplementary Info code
    Signed-off-by: mvglasow <michael -at- vonglasow.com>

--- RDSSurveyor/src/eu/jacquet80/rds/app/oda/AlertC.java
+++ RDSSurveyor/src/eu/jacquet80/rds/app/oda/AlertC.java
@@ -513,7 +513,9 @@ public class AlertC extends ODA {
 				break;
 				
 			case 6:
-				currentInformationBlock.currentEvent.suppInfo.add(TMC.SUPP_INFOS.get(value));
+				SupplementaryInfo si = TMC.SUPP_INFOS.get(value);
+				if (si != null)
+					currentInformationBlock.currentEvent.suppInfo.add(si);
 				break;
 				
 			case 7:
